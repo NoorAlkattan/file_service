cc89030f294e5d3d22d716aea2b5dcc3d63dddf9
NoorAlkattan <noor_samir@yahoo.com>
2017-03-14 11:31:45 -0500

    upload_successful

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::DocumentsController < ApplicationController
+	
+
 
 	def show
 		@document = Document.find(params[:id])
@@ -7,16 +9,17 @@ class Api::V1::DocumentsController < ApplicationController
 
 	def create
     @document = Document.new(document_params)
-    @document.save
-	end
+	  @document.save
+
+ end
 
 	private
     # Use callbacks to share common setup or constraints between actions.
     def set_document
       @document = Document.find(params[:id])
     end
-		    # Never trust parameters from the scary internet, only allow the white list through.
+		   # Never trust parameters from the scary internet, only allow the white list through.
     def document_params
 		params.require(:document).permit(:file)
-    end
- end
+  end
+end
