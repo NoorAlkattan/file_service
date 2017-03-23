eba189d43121b9a7ac50d9563e8d39cea75e7bc5
NoorAlkattan <noor_samir@yahoo.com>
2017-03-23 14:12:53 -0500

    generated_file_name

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -1,15 +1,20 @@
 include HelperMethods
 class Api::V1::DocumentsController < ApplicationController
-  
+
 	def show
 		@document = Document.find(params[:id])
 		render json: @document
 	end
 
-	def create
+
+def create
 	  begin
       @document = Document.new(document_params)
       file_data = document_params['file'].open
+			@document.original_file_name = File.basename(@document.file_url)
+			o = [(0..9), ('A'..'Z')].map(&:to_a).flatten
+      string = (0...5).map { o[rand(o.length)] }.join
+			@document.generated_file_name = string + @document.original_file_name
       @document.checksum = compute_digest(file_data)
       if @document.save
         render json: @document
@@ -18,7 +23,7 @@ class Api::V1::DocumentsController < ApplicationController
       end
     rescue Exception => e
       render json: e.message.inspect
-    end  
+    end
 	end
 
 	private
