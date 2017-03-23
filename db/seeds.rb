86b6641daf5696fe5d909b42b43ae2dfafb7284b
mwmaki <mwmaki2@gmail.com>
2017-03-22 19:59:04 -0500

    seeding clients names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,103 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+client_list=[ 'The MENTOR Network','NTN','Rush','Meritor','Wackenhut','BANNER PLAN ADMINISTRATION','Lovelace Health Plan','Humana','Heico','PrimeWest',
+'HOLDER BCBSMT','Banner Plan Administration','HOLDER FCHP - Takeover','HOLDER FCHP','Chartered Health Plan','Avera Health Plans, Inc.','Florida Hospital Healthcare System',
+'Priority Partners','Employer Health Programs','HOLDER PARAMOUNT SAAS','Paramount','Mount Carmel Health Plan','BCBS Nebraska Saas','Columbia United Providers- Takeover',
+'Columbia United Providers (CUP)','Community Health Plans of Washington (CHPW)','Humana Mass Tort','Hometown Health','Hometown Health Firefighters','Fidelis Care',
+'Fidelis Care (Medicare Members)','Humana','Indiana University Health Plans','Physicians Plus','Health Choice Arizona','Essence Healthcare',
+'The Health Plan of the Upper Ohio Valley','AmeriHealth Medicaid','AmeriHealth Medicare','Avera Saas','Community First Health Plans','Meridian Health Plan',
+'Community Health Options','SIHO','WPS Health Plan','Fallon Part D','Healthfirst','Advantage','KelseyCare','Health Alliance Plan (HAP)','ATRIO','AmeriHealth Caritas Iowa',
+'Health First Health Plans','Fallon Direct Billing','Johns Hopkins HealthCare','Blue Medicare Advantage','Blue Advantage HMO','United Healthcare C&S','Independent Care Health Plan',
+'Commonwealth Care Alliance','Health Care Service Corporation','MDwise Inc.','Excellus Health Plan','BCBS of AZ','Presbyterian Healthcare Services','Blue Shield of California',
+'Health New England','Mount Carmel Health Plan','Healthfirst NY','Fallon','Central Health Plan of California','Inter Valley Health Plan','Independence Blue Cross','Arkansas Blue Cross Blue Shield',
+'SummaCare','Elderplan','HealthChoice','Health Alliance Medical Plans','Martins Point','Meridian','Caremore','Anthem','Essence','Harvard Pilgrim Health Care','Vantage','SelectHealth',
+'AmeriHealth','Unity Gundersen','UPHP','Humana','Pacific Source','Indiana University Health Plans','MCS']
 
 
-cl_sk	cl_shortname
-2	The MENTOR Network
-3	NTN
-4	Rush
-5	Meritor
-6	Wackenhut
-7	BANNER PLAN ADMINISTRATION
-8	Lovelace Health Plan
-9	Humana
-10	Heico
-11	PrimeWest
-12	HOLDER BCBSMT
-13	Banner Plan Administration
-14	HOLDER FCHP - Takeover
-15	HOLDER FCHP
-16	Chartered Health Plan
-17	Avera Health Plans, Inc.
-18	Florida Hospital Healthcare System
-19	Priority Partners
-20	Employer Health Programs
-21	HOLDER PARAMOUNT SAAS
-22	Paramount
-23	Mount Carmel Health Plan
-24	BCBS Nebraska Saas
-25	Columbia United Providers- Takeover
-26	Columbia United Providers (CUP)
-27	Community Health Plans of Washington (CHPW)
-28	Humana Mass Tort
-29	Hometown Health
-30	Hometown Health Firefighters
-31	Fidelis Care
-32	Fidelis Care (Medicare Members)
-33	Humana
-34	Indiana University Health Plans
-35	Physicians Plus
-36	Health Choice Arizona
-37	Essence Healthcare
-38	The Health Plan of the Upper Ohio Valley
-39	AmeriHealth Medicaid
-40	AmeriHealth Medicare
-41	Avera Saas
-42	Community First Health Plans
-43	Meridian Health Plan
-44	Community Health Options
-45	SIHO
-46	WPS Health Plan
-47	Fallon Part D
-48	Healthfirst
-49	Advantage
-50	KelseyCare
-51	Health Alliance Plan (HAP)
-52	ATRIO
-53	AmeriHealth Caritas Iowa
-54	Health First Health Plans
-55	Fallon Direct Billing
-56	Johns Hopkins HealthCare
-57	Blue Medicare Advantage
-58	Blue Advantage HMO
-59	United Healthcare C&S
-60	Independent Care Health Plan
-61	Commonwealth Care Alliance
-62	Health Care Service Corporation
-63	MDwise Inc.
-64	Excellus Health Plan
-65	BCBS of AZ
-66	Presbyterian Healthcare Services
-67	Blue Shield of California
-68	Health New England
-69	Mount Carmel Health Plan
-70	Healthfirst NY
-71	Fallon
-72	Central Health Plan of California
-73	Inter Valley Health Plan
-74	Independence Blue Cross
-75	Arkansas Blue Cross Blue Shield
-76	SummaCare
-77	Elderplan
-78	HealthChoice
-79	Health Alliance Medical Plans
-80	Martins Point
-81	Meridian
-82	Caremore
-83	Anthem
-84	Essence
-85	Harvard Pilgrim Health Care
-86	Vantage
-87	SelectHealth
-88	AmeriHealth
-89	Unity Gundersen
-90	UPHP
-91	Humana
-92	Pacific Source
-93	Indiana University Health Plans
-94	MCS
+client_list.each do |name|
+  ClientBucket.create(client_name: name)
+end
