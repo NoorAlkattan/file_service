cb3060e59c019d4b75f44eb98b6b74ef6e2bb061
mwmaki <mwmaki2@gmail.com>
2017-03-23 11:49:52 -0500

    editing the show action to not show   if checksum not the same

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -1,13 +1,18 @@
 include ChecksumHelper
 class Api::V1::DocumentsController < ApplicationController
-  
-	def show
-		@document = Document.find(params[:id])
-		render json: @document
-	end
 
-	def create
-	  begin
+  def show
+    @document = Document.find(params[:id])
+    if params[:checksum] == @document.checksum
+      render json: @document
+    else
+      render json: {errors: "any message"}, status: 422
+    end
+  end
+
+
+  def create
+    begin
       @document = Document.new(document_params)
       file_data = document_params['file'].open
       @document.checksum = compute_digest(file_data)
@@ -18,11 +23,10 @@ class Api::V1::DocumentsController < ApplicationController
       end
     rescue Exception => e
       render json: e.message.inspect
-    end  
-	end
-
-	private
+    end
+  end
+  private
   def document_params
-	  params.require(:document).permit(:file)
+    params.require(:document).permit(:file)
   end
 end
