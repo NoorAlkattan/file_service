6cb04a5ce2924d3e3b3fbe26906b52eecdbf649d
Samer Sharba <samer.sharba@gmail.com>
2017-03-23 17:16:39 +0000

    filter response data

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -2,24 +2,26 @@ include HelperMethods
 class Api::V1::DocumentsController < ApplicationController
 
   def show
-    @document = Document.find(params[:id])
-    if params[:checksum] == @document.checksum
-      render json: @document
+    document = Document.find(params[:id])
+    if params[:checksum] == document.checksum
+      document_data = document.as_json(only: [:id, :file])
+      render json: document_data
     else
-      render json: {errors: "the information you provided dose not match any file"}, status: 422
+      render json: {errors: 'the information you provided dose not match any file'}, status: 422
     end
   end
 
 
   def create
     begin
-      @document = Document.new(document_params)
+      document = Document.new(document_params)
       file_data = document_params['file'].open
-      @document.checksum = compute_digest(file_data)
-      if @document.save
-        render json: @document
+      document.checksum = compute_digest(file_data)
+      if document.save
+        document_data = document.as_json(only: [:id, :checksum])
+        render json: document_data
       else
-        render json: @document.errors
+        render json: document.errors
       end
     rescue Exception => e
       render json: e.message.inspect
